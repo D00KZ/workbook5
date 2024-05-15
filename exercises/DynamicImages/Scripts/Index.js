4ce83dcdd27d93cbc813a40eeee2d26b732b1898
D00KZ <christopheraadookie@gmail.com>
2024-05-15 14:13:08 -0400

    DynamicImages Project V1

---
+++ exercises/DynamicImages/Scripts/Index.js
@@ -0,0 +1,42 @@
+"Use Strict";
+
+let imageFiles = [
+  { id: "1", path: "https://placehold.co/300x300", description: "placeholder 1" },
+  { id: "2", path: "https://placehold.co/300x300", description: "placeholder 2" },
+  { id: "3", path: "https://placehold.co/300x300", description: "placeholder 3" },
+  { id: "4", path: "https://placehold.co/300x300", description: "placeholder 4" },
+  { id: "5", path: "https://placehold.co/300x300", description: "placeholder 5" },
+  { id: "6", path: "https://placehold.co/300x300", description: "placeholder 6" },
+  { id: "7", path: "https://placehold.co/300x300", description: "placeholder 7" },
+  { id: "8", path: "https://placehold.co/300x300", description: "placeholder 8" },
+  { id: "9", path: "https://placehold.co/300x300", description: "placeholder 9" },
+  { id: "10", path: "https://placehold.co/300x300", description: "placeholder 10" },
+];
+
+window.onload = function () {
+  loadOptions();
+
+  function loadOptions() {
+    const displayDropdown = document.querySelector("#displayDropdown");
+
+    for (const imageFile of imageFiles) {
+      const option = document.createElement("option");
+      option.value = imageFile.id;
+      option.innerText = imageFile.id; //optional 
+      displayDropdown.appendChild(option);
+    }
+  }
+
+  function displayImageOptions() {
+    const displayDropdown = document.querySelector("#displayDropdown").value;
+    const selectedImage = imageFiles.find((image) => image.id === displayDropdown);
+
+    if (selectedImage) {
+      const imageDisplay = document.querySelector("#imageDisplay");
+      imageDisplay.innerHTML = `<img src="${selectedImage.path}" alt="${selectedImage.description}" />`;
+    }
+  }
+
+  const displayDropdownButton = document.querySelector("#displayDropdownButton");
+  displayDropdownButton.addEventListener("click", displayImageOptions);
+};
